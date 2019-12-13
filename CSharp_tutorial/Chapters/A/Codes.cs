afe2d9d8c6196d4b48397b3f036191656e5f395a
Ham-JH <52693438+Ham-JH@users.noreply.github.com>
2019-12-14 01:27:42 +0900

    A장 문자열 출력관리 코드 완료

--- CSharp_tutorial/Chapters/A/Codes.cs
+++ CSharp_tutorial/Chapters/A/Codes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -89,6 +90,78 @@ namespace CSharp_tutorial.Chapters.A
                             Console.WriteLine("{0}", element101);
                         }
 
+                    }
+                    break;
+
+                case 106:
+                    {
+                        string fmt106 = "{0, -20}{1,-15}{2,30}";
+
+                        Console.WriteLine(fmt106, "Publisher", "Author", "Title");
+                        Console.WriteLine(fmt106, "Marvel", "Stan Lee", "Iron Man");
+                        Console.WriteLine(fmt106, "Hanbit", "Sanghyun Park", "C# 7.0 Programming");
+                        Console.WriteLine(fmt106, "Prentice Hann", "K&R", "The C Programming Language");
+                    }
+                    break;
+
+                case 108:
+                    {
+                        // D : 10진수
+                        Console.WriteLine("10진수: {0:D}", 123);
+                        Console.WriteLine("10진수: {0:D5}", 123);
+
+                        // X : 16진수
+                        Console.WriteLine("16진수: {0:X}", 0xFF1234);
+                        Console.WriteLine("16진수: {0:X8}", 0xFF1234);
+
+                        // N : 숫자
+                        Console.WriteLine("숫자: {0:N}", 123456789);
+                        Console.WriteLine("숫자: {0:N0}", 123456789); // 자릿수 0은 소숫점 이하를 제거함.
+
+                        // F : 고정소수점
+                        Console.WriteLine("고정소수점: {0:F}", 123.45);
+                        Console.WriteLine("고정소수점: {0:F5}", 123.45);
+
+                        // E : 공학용
+                        Console.WriteLine("공학: {0:E}", 123.456789);
+                    }
+                    break;
+
+                case 111:
+                    {
+                        DateTime dt111 = new DateTime(2018, 11, 3, 23, 18, 22);
+
+                        Console.WriteLine("12시간 형식: {0:yyyy-MM-dd tt hh:mm:ss (ddd)}", dt111);
+                        Console.WriteLine("12시간 형식: {0:yyyy-MM-dd tt hh:mm:ss (ddd)}", dt111);
+
+                        CultureInfo ciKo111 = new CultureInfo("ko-KR");
+                        Console.WriteLine();
+                        Console.WriteLine(dt111.ToString("yyyy-MM-dd tt hh:mm:ss (ddd)", ciKo111));
+                        Console.WriteLine(dt111.ToString("yyyy-MM-dd HH:mm:ss (dddd)", ciKo111));
+                        Console.WriteLine(dt111.ToString(ciKo111));
+
+                        CultureInfo ciEn111 = new CultureInfo("en-US");
+                        Console.WriteLine();
+                        Console.WriteLine(dt111.ToString("yyyy-MM-dd tt hh:mm:ss (ddd)", ciEn111));
+                        Console.WriteLine(dt111.ToString("yyyy-MM-dd HH:mm:ss (ddd)", ciEn111));
+                        Console.WriteLine(dt111.ToString(ciEn111));
+
+                    }
+                    break;
+
+                case 114:
+                    {
+                        string name114 = "김튼튼";
+                        int age114 = 23;
+                        Console.WriteLine($"{name114,-10}, {age114:D3}");
+
+                        name114 = "박날씬";
+                        age114 = 30;
+                        Console.WriteLine($"{name114}, {age114,-10:D3}");
+
+                        name114 = "이비실";
+                        age114 = 17;
+                        Console.WriteLine($"{name114},{(age114 > 20 ? "성인" : "미성년자")}");
                     }
                     break;
             }
